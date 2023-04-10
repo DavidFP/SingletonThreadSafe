637ffd54d06849e0d51976454fead0d1217b05d0
David Fernández Puentes <dfernandezpuentes@gmail.com>
2023-04-11 00:31:38 +0200

    Initial Commit
    Example of Singleton thread safe in C#

---
+++ SingletonTest/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace SingletonTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejemplo de Singleton Multihilo");
+
+            Parallel.Invoke(
+                () => PrintSingletonInstance(),
+                () => PrintSingletonInstance(),
+                () => PrintSingletonInstance(),
+                () => PrintSingletonInstance(),
+                () => PrintSingletonInstance(),
+                () => PrintSingletonInstance(),
+                () => PrintSingletonInstance(),
+                () => PrintSingletonInstance()
+                );
+            Console.ReadKey();
+        }
+
+        static void PrintSingletonInstance()
+        {
+            Singleton singleton = Singleton.GetInstance();
+            Console.WriteLine($"La instancia del Singleton es: {singleton.GetHashCode()}");
+        }
+    }
+
+    public sealed class Singleton
+    {
+        private static volatile Singleton instance;
+        private static object syncRoot = new Object();
+
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (instance == null)
+                        instance = new Singleton();
+                }
+            }
+
+            return instance;
+        }
+    }
+}
